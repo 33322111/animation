ab4415ba699e0c6e0be795eabdf78869f787b1a8
33322111 <ievseev.100@mail.ru>
2021-02-18 21:19:55 +0300

    animation
    animation

---
+++ praktika_dot/praktika_dot/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace praktika_dot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            this.btnPaint = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAnimate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Canvas.Location = new System.Drawing.Point(-1, 1);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(783, 490);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
+            // 
+            // btnPaint
+            // 
+            this.btnPaint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPaint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPaint.Location = new System.Drawing.Point(13, 506);
+            this.btnPaint.Name = "btnPaint";
+            this.btnPaint.Size = new System.Drawing.Size(128, 43);
+            this.btnPaint.TabIndex = 1;
+            this.btnPaint.Text = "Нарисовать";
+            this.btnPaint.UseVisualStyleBackColor = true;
+            this.btnPaint.Click += new System.EventHandler(this.btnPaint_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClear.Location = new System.Drawing.Point(147, 506);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(128, 43);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Стереть";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnAnimate
+            // 
+            this.btnAnimate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAnimate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAnimate.Location = new System.Drawing.Point(281, 506);
+            this.btnAnimate.Name = "btnAnimate";
+            this.btnAnimate.Size = new System.Drawing.Size(128, 43);
+            this.btnAnimate.TabIndex = 3;
+            this.btnAnimate.Text = "Оживить";
+            this.btnAnimate.UseVisualStyleBackColor = true;
+            this.btnAnimate.Click += new System.EventHandler(this.btnAnimate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnAnimate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPaint);
+            this.Controls.Add(this.Canvas);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Поехали!!!";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.Button btnPaint;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAnimate;
+    }
+}
+
---
+++ praktika_dot/praktika_dot/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace praktika_dot
+{
+    public partial class Form1 : Form
+    {
+        int width, height; 
+        static Graphics g;
+        static Timer moveTimer;
+        int dotX, dotY;   
+        int dotR = 10;
+        int deltaX = 5;
+        int deltaY = 5;
+        Color dotColor = Color.Black;
+        Color foreColor = Color.AliceBlue;
+        SolidBrush dotBrush;     
+        SolidBrush foreBrush;      
+        bool animate = false;
+        bool paint = false;
+        int timerInterval = 20;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            width = Canvas.Width;
+            height = Canvas.Height;
+            Canvas.Image = new Bitmap(width, height);
+            g = Graphics.FromImage(Canvas.Image);
+            dotX = width / 30;
+            dotY = height / 2;
+            dotBrush = new SolidBrush(dotColor);
+            foreBrush = new SolidBrush(foreColor);
+            g.FillRectangle(foreBrush, 0, 0, width, height);
+            moveTimer = new Timer();        
+            moveTimer.Interval = timerInterval;     
+            moveTimer.Tick += MoveTimer_Tick;                      
+        }
+
+        private void PaintDot()       
+        {
+            g.FillEllipse(dotBrush, dotX - dotR, dotY - dotR, dotR * 2, dotR * 2);
+        }
+
+        private void ClearDot()        
+        {
+            g.FillEllipse(foreBrush, dotX - dotR - 1, dotY - dotR - 1, dotR * 2 + 2, dotR * 2 + 2);
+        }
+
+        private void MoveTimer_Tick(object sender, EventArgs e)
+        {
+            ClearDot();   
+
+            if (dotX + deltaX >= width - dotR || dotX + deltaX <= dotR)            
+                deltaX *= -1;
+            if (dotY + deltaY >= height - dotR || dotY + deltaY <= dotR)
+                deltaY *= -1;
+
+            dotX += deltaX;
+            dotY += deltaY;
+
+            PaintDot();
+
+            Canvas.Refresh();
+        }
+
+        private void btnPaint_Click(object sender, EventArgs e)
+        {
+            if (paint) return; 
+            
+            PaintDot();
+            Canvas.Refresh();
+            paint = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (!paint) return;   
+
+            if (animate)
+            {
+                MessageBox.Show("Сначала остановите точку!");
+                return;
+            }
+
+            ClearDot();
+            Canvas.Refresh();
+            paint = false;
+        }
+
+        private void btnAnimate_Click(object sender, EventArgs e)
+        {
+            if (!paint)
+            {
+                MessageBox.Show("Для анимации точка должна быть отрисована!");
+                return;
+            }
+
+            if (!animate)
+            {
+                moveTimer.Start();     
+                animate = true;
+                btnAnimate.Text = "Остановить";
+            }
+            else
+            {
+                moveTimer.Stop();
+                animate = false;
+                btnAnimate.Text = "Оживить";
+            }
+        }
+    }
+}
